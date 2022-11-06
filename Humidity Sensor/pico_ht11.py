5bf36e73e8be2478358a07e2b09fa820847576d0
Saaketh Jasti <saakethkjasti@outlook.com>
2022-11-06 16:55:15 -0600

    Add Humidity & Temperature Sensor
    The HT11 sensor code and the supporting library (dht.py) have been added.

---
+++ Humidity Sensor/pico_ht11.py
@@ -0,0 +1,17 @@
+# This program is using the Raspberry Pi Pico to gather data using a simple tilt 
+# This was written using MicroPython version 1.19.1
+
+from machine import Pin
+import utime as time
+from dht import DHT11, InvalidChecksum
+
+# Connect the Pico's GP1 to ht11 sensor's DAT pin.
+# Connect the Pico's 3.3V Pin to ht11 sensor's VCC pin.
+# Connect the Pico's GND pin to ht11 sensor's GND pin.
+dhtPIN = 1
+dhtSensor = DHT11(Pin(dhtPIN, Pin.OUT, Pin.PULL_DOWN))
+
+# Print the temperature followed by the humidity
+while True:
+    print("Temp: {}°C".format(dhtSensor.temperature), "| Hum: {0:.1%}".format(dhtSensor.humidity/100))
+    time.sleep(1.1)
