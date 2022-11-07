5383734cfca05687064fa14e68a2169f0c2e10d2
Luistorres5 <72368136+Luistorres5@users.noreply.github.com>
2022-11-06 23:00:40 -0600

    Update PIR_Sensor.py
    I used a bit of Chris's code to get mine working, will try to continue to improve on the algorithm and/or sensor calibration to see if I can improve the performance of the data capturing and sensing

--- PIR SENSOR/PIR_Sensor.py
+++ PIR SENSOR/PIR_Sensor.py
@@ -1,17 +1,61 @@
+#import statements
+import rp2
 from machine import Pin
 from time import sleep
+import utime
+import time
 import sys
 
+#establishing/displaying sytem information
+print("System info: ")
+print (str(sys.implementation))
+print()
+print()
+
+#displaying and setting the time
+print("System time: ")
+rtc = machine.RTC()
+timestamp = rtc.datetime()
+timestring="%04d-%02d-%02d %02d:%02d:%02d"%(timestamp[0:3] + timestamp[4:7])
+print("Date and time: " + timestring)
+print()
+print()
+
+#creating and opening file to store sensor data
+file = open ("sensordata.txt", "w")
+
+#assigning pin values and setting up sensor
 led = Pin(0,Pin.OUT)
 PIR_Sensor = Pin(1, Pin.IN)
 
+#setting up temp to record
+sensorTemp = machine.ADC(machine.ADC.CORE_TEMP)
+conversionFactor = 3.3/ (65535)
+read = sensorTemp.read_u16() * conversionFactor
+temp = 27 - (read - 0.706)/0.001721
+
+
+
+#continuous run loop
+led.value(0)
 while True:
+    file = open ("sesnsordata.txt", "w")
+    file.read()
+    file.write(timestring + ", temp: " + str(temp) + ' C ' + "\n")
+    file.close()
+    
     if PIR_Sensor.value()==0:
-        print("human not detected")
+        print("human not detected and the ambient temp is " + str(temp) + " degrees Celsius.")
+        #file.write("human not detected and the ambient temp is " + str(temp))
         led.value(0)
         sleep(0.05)
     else:
-        print("human detected")
+        print("human detected and the temperature is " + str(read) + "degrees celsius.")
         led.value(1)
         sleep(0.05)
+        
+#file.close()
+
+#if you wish to write out data lingering in program to the actual file, use:
+#file.flush()
 
