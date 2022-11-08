45b7ebfcd274a7e77bf93a0a9c579432fa1f0bfd
Swaraaj <60723983+swarj@users.noreply.github.com>
2022-11-08 10:50:56 -0600

    Update ls.py

--- Light_Sensor/ls.py
+++ Light_Sensor/ls.py
@@ -6,7 +6,7 @@ import utime
 import time
 import sys
  
-# NOTES - THE LIGHT INTENSITY LEVEL IS ADJUSTED BY THE SCREW ON THE BLUE BOX OF THE SENSOR
+# NOTES - I was able to adjust the light intensity via the blue box in the sensor
  
 #establishing/displaying sytem information
 print("System info: ")
@@ -29,7 +29,7 @@ file = open ("sensordata.txt", "w")
  
 ## This first section can check if the Light is Above or Below a certain threshold
 ## The light sensor value can be tweaked to print/output a specific message based on
-## The amount of light
+## The amount of light. 1 Represents a high analog signal, and 0 a low signal
  
 # light_sensor = Pin(0,Pin.IN)
 # 
@@ -56,11 +56,13 @@ file = open ("sensordata.txt", "w")
     
 
 # take the light sensor and via an analog reading and some math,
-# convert it to a percentage
+# convert it to a percentage- a more readble understanding
+# and probably the most useful case when coordinated with a timestamp
+# and sent into a file
 
 photoPIN = 27
 def readLight(photoGP):
-    photoRes = ADC(Pin(27))
+    photoRes = ADC(Pin(photoPIN))
     light = photoRes.read_u16()
     light = round(light/65535*100,2)
     return light
@@ -74,3 +76,10 @@ while True:
     file.close()
     print("light: " + str(readLight(photoPIN)) +"%")
     sleep(2) # set a delay between readings
+
+    
+# SAMPLE OUTPUT
+#light: 17.31%
+#light: 19.29%
+#light: 5.39%
+#light: 5.19%
