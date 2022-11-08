61230c2ccbd6d02e17860a92f1743f0af811bb14
divya-g-248 <57281528+divya-g-248@users.noreply.github.com>
2022-11-07 21:35:57 -0600

    Update soundSensor.py

--- Sound Sensor/soundSensor.py
+++ Sound Sensor/soundSensor.py
@@ -51,18 +51,3 @@ while (True):
         else:
             led.value(0)
         sleep(0.5)
-
-'''
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BOARD) # use board numbers
-# define input pin
-pin = 12
-GPIO.setup(pin, GPIO.IN)
-
-while 1:
-  if GPIO.input(pin) == GPIO.LOW:
-    print ("i hear you")
-    time.sleep(1)
-'''
-
