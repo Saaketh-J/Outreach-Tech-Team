d418fb02d2e5be2230ae5905da7b7283b647fd4b
Luistorres5 <72368136+Luistorres5@users.noreply.github.com>
2022-11-14 14:25:30 -0600

    Update PIR_Sensor.py

--- PIR SENSOR/PIR_Sensor.py
+++ PIR SENSOR/PIR_Sensor.py
@@ -39,25 +39,45 @@ temp = 27 - (read - 0.706)/0.001721
 
 #continuous run loop that is the main program
 led.value(0)
+count = 0
 while True:
     file = open ("sesnsordata.txt", "w")
     file.read()
     file.write(timestring + ", temp: " + str(temp) + ' C ' + "\n")
     file.close()
     #print("Times activity detected: " + str(count))
-    
+     
+    if count >=50:
+        print("Real interaction detected")
+        file = open ("count.txt", "a")
+        file.write("human detected with a temperature of " + str(temp) + " C° at " + str(timestring) + "\n" )
+        file.close()
+        count = 0
+#    if count < 20 and count >18:
+#        count = 0
+     
+    #no movement detected
     if PIR_Sensor.value()==0:
         led.value(0)
-        print("No activity detected.")
-
-        
+   #     print("No activity detected.")  
         #sleep(0.01)
+   
+    #detecting movement
     else:
         led.value(1)
-        print("human detected and the temperature is " + str(temp) + "degrees celsius.")
-        file = open ("sensorvalues.txt", "w")
-        file.write("human detected and the ambient temp is " + str(temp) + " at " + str(timestring))
+    #    print("human detected and the temperature is " + str(temp) + "degrees celsius.")
+        file = open ("sensorvalues.txt", "a")
+        file.write("human detected and the ambient temp is " + str(temp) + " at " + str(timestring) + "\n")
         file.close()
+        count +=1
+        print(count)
+        
+        
+        
+            
+        
+
+        
         #count++
         
         
@@ -68,5 +88,7 @@ while True:
 
 #if you wish to write out data lingering in program to the actual file, use:
 #file.flush()
+
+
 
 
